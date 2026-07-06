28f9ac8085f8894a3fa5f18a082f0745f4302e7e
Xarvex <dev.ellz6@xarvex.simplelogin.com>
2026-07-06 14:16:20 -0500

    ci

--- scripts/semver-segments.pl
+++ scripts/semver-segments.pl
@@ -7,7 +7,7 @@ use warnings;
 use feature 'say';
 
 # From: https://regex101.com/r/vkijKf/1
-if (<STDIN> =~ /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/) {
+if (($ARGV[0] // <STDIN>) =~ /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/) {
     foreach my $index (0 .. $#{^CAPTURE}) {
         say ${^CAPTURE}[$index] // "";
     }
